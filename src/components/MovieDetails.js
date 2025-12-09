84a98b50e4d584f62c63333cbe4dd6be15b1f446
koro <korochi299@gmail.com>
2025-12-09 14:15:47 +0100

    Update MovieDetails.js

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -65,7 +65,7 @@ export function MovieDetails({
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         console.log(data);
